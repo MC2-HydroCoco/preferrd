4e95f9e739c33866aed780109599e19347201566
Christianto Budisaputra <53547157+chrsomle@users.noreply.github.com>
2021-05-29 01:15:31 +0700

    Create String extension to access localizable string(s)

---
+++ hydrococo/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  hydrococo
+//
+//  Created by Christianto Budisaputra on 29/05/21.
+//
+
+import Foundation
+
+extension String {
+  // Call on any string and it'll return a translated string, if any.
+  /// ex: let hello = "Hello World".localized
+  /// if any translation for "Hello World" is specified,
+  /// then, based on the device's language, you'll get the translated string.
+  var localized: String { NSLocalizedString(self, comment: "") }
+}
