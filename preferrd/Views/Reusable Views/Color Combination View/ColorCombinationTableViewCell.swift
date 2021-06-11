f0365befb6176723e8df6db93c2a034e6ae350f8
Christianto Budisaputra <53547157+chrsomle@users.noreply.github.com>
2021-06-11 16:08:16 +0700

    Create Color and Color Combination cells

---
+++ preferrd/Views/Reusable Views/Color Combination View/ColorCombinationTableViewCell.swift
@@ -0,0 +1,41 @@
+//
+//  ColorCombinationTableViewCell.swift
+//  preferrd
+//
+//  Created by Michelle Linus on 10/06/21.
+//
+
+import UIKit
+
+class ColorCombinationTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var colorCombinationBakcground: UIView!
+    @IBOutlet weak var colorBox1: UIView!
+    @IBOutlet weak var colorBox2: UIView!
+    @IBOutlet weak var colorBox3: UIView!
+    @IBOutlet weak var colorBox4: UIView!
+    @IBOutlet weak var colorBox5: UIView!
+    @IBOutlet weak var colorCombinationName: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        colorCombinationBakcground.layer.cornerRadius = 12
+        colorBox1.layer.cornerRadius = 12
+        colorBox2.layer.cornerRadius = 12
+        colorBox3.layer.cornerRadius = 12
+        colorBox4.layer.cornerRadius = 12
+        colorBox5.layer.cornerRadius = 12
+        
+    
+
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    @IBAction func colorcombinationInformation(_ sender: UIButton) {
+    }
+}
---
+++ preferrd/Views/Reusable Views/Color Table View /ColorTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  ColorTableViewCell.swift
+//  preferrd
+//
+//  Created by Michelle Linus on 10/06/21.
+//
+
+import UIKit
+
+class ColorTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var colorBox: UIView!
+    @IBOutlet weak var colorName: UILabel!
+    @IBOutlet weak var colorHex: UILabel!
+    @IBOutlet weak var colorBackground: UIView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+      
+        colorBackground.layer.cornerRadius = 12
+        colorBox.layer.cornerRadius = 12
+    }
+
+    @IBAction func colorInformation(_ sender: UIButton) {
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+ 
+    }
+    
+}
