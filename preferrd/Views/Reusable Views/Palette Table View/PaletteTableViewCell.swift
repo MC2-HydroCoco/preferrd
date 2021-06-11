93d2d92a844881860c847000f37c831ed7b75693
Christianto Budisaputra <53547157+chrsomle@users.noreply.github.com>
2021-06-11 17:58:42 +0700

    Create Palette table view

---
+++ preferrd/Views/Reusable Views/Palette Table View/PaletteTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  PaletteTableViewCell.swift
+//  preferrd
+//
+//  Created by Fandika Ikhsan on 10/06/21.
+//
+
+import UIKit
+
+class PaletteTableViewCell: UITableViewCell {
+
+    @IBOutlet var labelPalette: UILabel!
+    @IBOutlet var colorPallete: UIView!
+    @IBOutlet var colorPalette2: UIView!
+    @IBOutlet var colorPalette3: UIView!
+    @IBOutlet var colorPalette4: UIView!
+    @IBOutlet var colorPalette5: UIView!
+
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
