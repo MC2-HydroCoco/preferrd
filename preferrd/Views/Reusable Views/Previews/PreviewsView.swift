0a18a2f813c62c0c3fd2626210c6285857a9bc24
Christianto Budisaputra <53547157+chrsomle@users.noreply.github.com>
2021-06-11 18:03:56 +0700

    Create Preview view

---
+++ preferrd/Views/Reusable Views/Previews/PreviewsView.swift
@@ -0,0 +1,25 @@
+//
+//  PreviewsView.swift
+//  preferrd
+//
+//  Created by Fandika Ikhsan on 11/06/21.
+//
+
+import UIKit
+
+class PreviewsView: UIView {
+    @IBOutlet var tittleLabel: UILabel!
+    @IBOutlet var captionLabel: UILabel!
+    @IBOutlet var buttonLabel: UIButton!
+
+    @IBOutlet var backgroundView: UIView!
+    @IBOutlet var buttonBackgroundView: UIView!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
