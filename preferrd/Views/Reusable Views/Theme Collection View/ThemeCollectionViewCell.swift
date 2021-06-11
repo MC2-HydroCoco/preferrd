0aec21e1777bf809d2f325c6699100ed35abd977
Christianto Budisaputra <53547157+chrsomle@users.noreply.github.com>
2021-06-11 16:17:05 +0700

    Create Theme Collection View cell

---
+++ preferrd/Views/Reusable Views/Theme Collection View/ThemeCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ThemeCollectionViewCell.swift
+//  preferrd
+//
+//  Created by Antonio Mario on 10/06/21.
+//
+
+import UIKit
+
+class ThemeCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var themeContainer: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
