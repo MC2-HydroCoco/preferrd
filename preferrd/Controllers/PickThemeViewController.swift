5b558f6f507cea9dfba3cef553088a50b709da7e
maarioio <81684269+maarioio@users.noreply.github.com>
2021-06-14 15:20:55 +0700

    Re-arrange pick theme storyboard

--- preferrd/Controllers/PickThemeViewController.swift
+++ preferrd/Controllers/PickThemeViewController.swift
@@ -7,7 +7,11 @@
 
 import UIKit
 
-class PickThemeViewController: UIViewController {
+class PickThemeViewController: UIViewController, UICollectionViewDataSource {
+    
+    @IBOutlet weak var themeVerticalStack: UIStackView!
+    
+//    let themes = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
