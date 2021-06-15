61347f3e87e6bedae43c949ac62a658257e676fc
tim-nnd <timothy.ananda@gmail.com>
2021-06-15 16:21:01 +0700

    Add enums for ColorGroup

---
+++ preferrd/Models/BaseColorGroup.swift
@@ -0,0 +1,41 @@
+//
+//  BaseColorGroup.swift
+//  preferrd
+//
+//  Created by Timothy Ananda on 15/06/21.
+//
+
+import Foundation
+
+enum BaseColorGroup: String, CaseIterable {
+    case red         = "Red",
+         lightRed    = "Light Red",
+         darkRed     = "Dark Red",
+         brown       = "Brown",
+         lightBrown  = "Light Brown",
+         darkBrown   = "Dark Brown",
+         orange      = "Orange",
+         lightOrange = "Light Orange",
+         darkOrange  = "Dark Orange",
+         yellow      = "Yellow",
+         lightYellow = "Light Yellow",
+         green       = "Green",
+         lightGreen  = "Light Green",
+         darkGreen   = "Dark Green",
+         blue        = "Blue",
+         lightBlue   = "Light Blue",
+         darkBlue    = "Dark Blue",
+         purple      = "Purple",
+         lightPurple = "Light Purple",
+         darkPurple  = "Dark Purple",
+         white       = "White",
+         black       = "Black"
+    
+    func getRawBaseColor() -> String {
+        return self.rawValue.suffix(1)
+    }
+    
+    func getColorMeaning() -> String {
+        
+    }
+}
--- preferrd/Models/ColorTheme.swift
+++ preferrd/Models/ColorTheme.swift
@@ -9,17 +9,17 @@ import Foundation
 import UIKit
 
 enum ColorTheme: String, CaseIterable {
-  case relaxing     = "Relaxing",
-       vintage      = "Vintage",
-       happy        = "Happy",
-       adventurous  = "Adventurous",
-       modern       = "Modern",
-       nature       = "Nature",
-       fresh        = "Fresh",
-       elegant      = "Elegant",
-       feminine     = "Feminine",
-       masculine    = "Masculine"
-
+    case relaxing     = "Relaxing",
+         vintage      = "Vintage",
+         happy        = "Happy",
+         adventurous  = "Adventurous",
+         modern       = "Modern",
+         nature       = "Nature",
+         fresh        = "Fresh",
+         elegant      = "Elegant",
+         feminine     = "Feminine",
+         masculine    = "Masculine"
+    
     func getImage() -> UIImage {
         switch self {
         case .relaxing:
@@ -29,7 +29,7 @@ enum ColorTheme: String, CaseIterable {
         }
     }
     
-  static func getRelatedColors(for meaning: ColorTheme) -> [Color] {
-    return Constants.colors.filter { $0.relatedTags.contains(meaning) }
-  }
+    static func getRelatedColors(for meaning: ColorTheme) -> [Color] {
+        return Constants.colors.filter { $0.relatedTags.contains(meaning) }
+    }
 }
