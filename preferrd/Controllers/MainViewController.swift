4dd51051f212765df07d9dba89897ce4e860a1ec
Myska David <81741505+myskadavid17@users.noreply.github.com>
2021-06-15 18:03:38 +0700

    Main Storyboard

--- preferrd/Controllers/MainViewController.swift
+++ preferrd/Controllers/MainViewController.swift
@@ -7,10 +7,31 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UITableViewDataSource {
+    
+    
 
-  override func viewDidLoad() {
+    @IBOutlet weak var userPaletteTable: UITableView!
+    
+    override func viewDidLoad() {
     super.viewDidLoad()
-  }
-
-}
+        let nibCell = UINib(nibName: "PaletteTableViewCell", bundle: nil)
+        userPaletteTable.register(nibCell, forCellReuseIdentifier: "userPaletteTableViewCell")
+        
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "userPaletteTableViewCell") as! PaletteTableViewCell
+//        cell.colorPalette2
+//        cell.colorPalette3
+//        cell.colorPalette4
+//        cell.colorPalette5
+//        cell.colorPallete
+//        cell.labelPalette
+        
+        }
+        
+    }
