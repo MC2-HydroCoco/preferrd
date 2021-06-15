83d54ca637ff0957c4e2de19f5c811275017ae07
tim-nnd <timothy.ananda@gmail.com>
2021-06-15 13:41:18 +0700

    Modify Color Constants

--- preferrd/Models/Color.swift
+++ preferrd/Models/Color.swift
@@ -3,6 +3,7 @@ import Foundation
 struct Color {
   let name, hex, colorDescription: String
   let relatedTags: [ColorMeaning]
+//    let colorRange: [ColorRange]
 
   init(name: String, hex: String, colorDescription: String = "", relatedTags: [ColorMeaning]) {
     self.name = name
