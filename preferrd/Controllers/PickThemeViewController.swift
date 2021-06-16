d111a6a50549c242e689c573a4465fe5a3999895
maarioio <81684269+maarioio@users.noreply.github.com>
2021-06-17 00:02:10 +0700

    add code to auto-resize theme container to phone size

--- preferrd/Controllers/PickThemeViewController.swift
+++ preferrd/Controllers/PickThemeViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 class PickThemeViewController: UIViewController {
   @IBOutlet weak var tagCollectionView: UICollectionView!
   @IBOutlet weak var themeCollectionView: UICollectionView!
-  //    @IBOutlet weak var themeVerticalStack: UIStackView!
 
   var selectedTheme = [ColorTheme]() {
     didSet {
@@ -23,53 +22,74 @@ class PickThemeViewController: UIViewController {
 
     let nibCell = UINib(nibName: "\(ThemeCollectionViewCell.self)", bundle: nil)
     themeCollectionView.register(nibCell, forCellWithReuseIdentifier: "themeCollectionViewCell")
-    
+//    let nibCellTag = UINib(nibName: "\(ThemeCollectionViewCell.self)", bundle: nil)
     tagCollectionView.register(nibCell, forCellWithReuseIdentifier: "tagCollectionViewCell")
     
     themeCollectionView.delegate = self
     themeCollectionView.dataSource = self
+    themeCollectionView.collectionViewLayout = UICollectionViewFlowLayout()
+    
     tagCollectionView.delegate = self
     tagCollectionView.dataSource = self
   }
 
 }
 
-extension PickThemeViewController: UICollectionViewDataSource, UICollectionViewDelegate {
-  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    if collectionView == tagCollectionView {
-      return selectedTheme.count
-    } else {
-      return ColorTheme.allCases.count
-    }
-  }
-
-  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    if let cell = collectionView.dequeueReusableCell(
-        withReuseIdentifier: "themeCollectionViewCell",
-        for: indexPath) as? ThemeCollectionViewCell {
-      let getColorTheme = ColorTheme.allCases[indexPath.row]
-      
+extension PickThemeViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if collectionView == tagCollectionView {
-        cell.themeContainer.layer.cornerRadius = 12
-        cell.themeLabel.text = getColorTheme.rawValue
-        cell.imageContainer.isHidden = true
+            return selectedTheme.count
             
         } else {
-        cell.removeButton.isHidden = true
-        cell.themeContainer.layer.cornerRadius = 12
-        cell.themeLabel.text = getColorTheme.rawValue
-        cell.imageContainer.image = getColorTheme.getImage()
+            return ColorTheme.allCases.count
         }
-      return cell
     }
-    return UICollectionViewCell()
-  }
-
-  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-    if collectionView == tagCollectionView {
-
-    } else {
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: "themeCollectionViewCell",
+            for: indexPath) as? ThemeCollectionViewCell {
+            
+            let getColorTheme = ColorTheme.allCases[indexPath.row]
+            
+            if collectionView == tagCollectionView {
+                cell.themeContainer.layer.cornerRadius = 12
+                cell.themeLabel.text = getColorTheme.rawValue
+                cell.imageContainer.isHidden = true
+                
+            } else {
+                cell.removeButton.isHidden = true
+                cell.themeContainer.layer.cornerRadius = 12
+                cell.themeLabel.text = getColorTheme.rawValue
+                cell.imageContainer.image = getColorTheme.getImage()
+                
+            }
+            return cell
+            
+        }
+        return UICollectionViewCell()
     }
-  }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let noOfCellsInRow = 6
+
+        let flowLayout = collectionViewLayout as! UICollectionViewFlowLayout
+        let totalSpace = flowLayout.sectionInset.left
+                + flowLayout.sectionInset.right
+                + (flowLayout.minimumInteritemSpacing * CGFloat(noOfCellsInRow - 1))
+        let screenBounds = UIScreen.main.bounds
+        let width = (screenBounds.width / 2.36) - totalSpace / CGFloat(noOfCellsInRow)
+        let height = (screenBounds.height / 9.431) - totalSpace / CGFloat(noOfCellsInRow)
+        
+        return CGSize(width: width, height: height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if collectionView == tagCollectionView {
+            
+        } else {
+            
+        }
+    }
 }
