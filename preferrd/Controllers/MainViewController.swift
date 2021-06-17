f34dc6580ce00a9be1f1cb033a99626dfc1cbcdd
tim-nnd <timothy.ananda@gmail.com>
2021-06-17 17:58:26 +0700

    Add Palette tableview to Main View and modify Palette XiB

--- preferrd/Controllers/MainViewController.swift
+++ preferrd/Controllers/MainViewController.swift
@@ -10,27 +10,48 @@ import CoreData
 
 class MainViewController: UIViewController {
     
-    @IBOutlet weak var userPaletteTable: UITableView!
+    @IBOutlet weak var paletteTableView: UITableView!
     
     var palettes: [Palette] = []
+    let paletteManager = PaletteManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        paletteTableView.delegate = self
+        paletteTableView.dataSource = self
         
+        let nibCell = UINib(nibName: "\(PaletteTableViewCell.self)", bundle: nil)
+        paletteTableView.register(nibCell, forCellReuseIdentifier: "paletteTableViewCell")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
-        let nibCell = UINib(nibName: "PaletteTableViewCell", bundle: nil)
-        userPaletteTable.register(nibCell, forCellReuseIdentifier: "userPaletteTableViewCell")
+//        paletteManager.add(name: "Sample Palette", backgroundHex: "#FFFFFF", headlineHex: "#212121", bodyHex: "#303030", buttonBgHex: "#121212", buttonTextHex: "#FFFFFF")
+        palettes = paletteManager.fetch()
     }
 }
 
 extension MainViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        palettes.count
+        return palettes.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "paletteTableViewCell") as! PaletteTableViewCell
+        
+        let palette = palettes[indexPath.row]
+        print(palette)
+        
+        cell.nameLabel.text = palette.name
+        cell.backgroundColorView.backgroundColor = UIColor(hex: palette.backgroundHex!)
+        cell.headlineColorView.backgroundColor = UIColor(hex: palette.headlineHex!)
+        cell.bodyColorView.backgroundColor = UIColor(hex: palette.bodyHex!)
+        cell.buttonBgView.backgroundColor = UIColor(hex: palette.buttonBgHex!)
+        cell.buttonTextView.backgroundColor = UIColor(hex: palette.buttonTextHex!)
+        
+        return cell
     }
     
 }
--- preferrd/Views/Reusable Views/Palette Table View/PaletteTableViewCell.swift
+++ preferrd/Views/Reusable Views/Palette Table View/PaletteTableViewCell.swift
@@ -9,16 +9,42 @@ import UIKit
 
 class PaletteTableViewCell: UITableViewCell {
 
-    @IBOutlet var labelPalette: UILabel!
-    @IBOutlet var colorPallete: UIView!
-    @IBOutlet var colorPalette2: UIView!
-    @IBOutlet var colorPalette3: UIView!
-    @IBOutlet var colorPalette4: UIView!
-    @IBOutlet var colorPalette5: UIView!
+    @IBOutlet weak var paletteBackgroundView: UIView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var backgroundColorView: UIView!
+    @IBOutlet var headlineColorView: UIView!
+    @IBOutlet var bodyColorView: UIView!
+    @IBOutlet var buttonBgView: UIView!
+    @IBOutlet var buttonTextView: UIView!
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        paletteBackgroundView.layer.cornerRadius = 12
+        paletteBackgroundView.layer.shadowColor = UIColor.black.cgColor
+        paletteBackgroundView.layer.shadowOpacity = 0.4
+        paletteBackgroundView.layer.shadowOffset = CGSize(width: 4, height: 4)
+        paletteBackgroundView.layer.shadowRadius = 5
+        
+        backgroundColorView.layer.cornerRadius = 12
+        backgroundColorView.layer.borderColor = UIColor.black.cgColor
+        backgroundColorView.layer.borderWidth = 0.2
+        
+        headlineColorView.layer.cornerRadius = 12
+        headlineColorView.layer.borderColor = UIColor.black.cgColor
+        headlineColorView.layer.borderWidth = 0.2
+        
+        bodyColorView.layer.cornerRadius = 12
+        bodyColorView.layer.borderColor = UIColor.black.cgColor
+        bodyColorView.layer.borderWidth = 0.2
+        
+        buttonBgView.layer.cornerRadius = 12
+        buttonBgView.layer.borderColor = UIColor.black.cgColor
+        buttonBgView.layer.borderWidth = 0.2
+        
+        buttonTextView.layer.cornerRadius = 12
+        buttonTextView.layer.borderColor = UIColor.black.cgColor
+        buttonTextView.layer.borderWidth = 0.2
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
